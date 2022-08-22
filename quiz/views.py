ddc9cba10fcb5eef748e15268ae97d4f5ef353ae
89wozniak@gmail.com <89wozniak@gmail.com>
2022-08-22 22:49:31 +0200

    mit allen Seiten

--- quiz/views.py
+++ quiz/views.py
@@ -2,3 +2,162 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'quiz/home.html')
+
+def schwarz(request):
+    return render(request, 'quiz/schwarz.html')
+
+def schwarzfehler1(request):
+    return render(request, 'quiz/schwarzfehler1.html')
+
+def schwarzfehler2(request):
+    return render(request, 'quiz/schwarzfehler2.html')
+
+def schwarzfehler3(request):
+    return render(request, 'quiz/schwarzfehler3.html')
+
+def schwarz2(request):
+    return render(request, 'quiz/schwarz2.html')
+
+def schwarz2fehler1(request):
+    return render(request, 'quiz/schwarz2fehler1.html')
+
+def schwarz2fehler2(request):
+    return render(request, 'quiz/schwarz2fehler2.html')
+
+def schwarz2fehler3(request):
+    return render(request, 'quiz/schwarz2fehler3.html')
+
+def schwarz3(request):
+    return render(request, 'quiz/schwarz3.html')
+
+def schwarz3fehler1(request):
+    return render(request, 'quiz/schwarz3fehler1.html')
+
+def schwarz3fehler2(request):
+    return render(request, 'quiz/schwarz3fehler2.html')
+
+def schwarz3fehler3(request):
+    return render(request, 'quiz/schwarz3fehler3.html')
+
+def schwarz4(request):
+    return render(request, 'quiz/schwarz4.html')
+
+def schwarz4fehler1(request):
+    return render(request, 'quiz/schwarz4fehler1.html')
+
+def schwarz4fehler2(request):
+    return render(request, 'quiz/schwarz4fehler2.html')
+
+def schwarz4fehler3(request):
+    return render(request, 'quiz/schwarz4fehler3.html')
+
+def schwarz5(request):
+    return render(request, 'quiz/schwarz5.html')
+
+def schwarz5fehler1(request):
+    return render(request, 'quiz/schwarz5fehler1.html')
+
+def schwarz5fehler2(request):
+    return render(request, 'quiz/schwarz5fehler2.html')
+
+def schwarz5fehler3(request):
+    return render(request, 'quiz/schwarz5fehler3.html')
+
+def gruen(request):
+    return render(request, 'quiz/gruen.html')
+
+def gruenfehler1(request):
+    return render(request, 'quiz/gruenfehler1.html')
+
+def gruenfehler2(request):
+    return render(request, 'quiz/gruenfehler2.html')
+
+def gruenfehler3(request):
+    return render(request, 'quiz/gruenfehler3.html')
+
+def gruen2(request):
+    return render(request, 'quiz/gruen2.html')
+
+def gruen2fehler1(request):
+    return render(request, 'quiz/gruen2fehler1.html')
+
+def gruen2fehler2(request):
+    return render(request, 'quiz/gruen2fehler2.html')
+
+def gruen2fehler3(request):
+    return render(request, 'quiz/gruen2fehler3.html')
+
+def gruen3(request):
+    return render(request, 'quiz/gruen3.html')
+
+def gruen3fehler1(request):
+    return render(request, 'quiz/gruen3fehler1.html')
+
+def gruen3fehler2(request):
+    return render(request, 'quiz/gruen3fehler2.html')
+
+def gruen3fehler3(request):
+    return render(request, 'quiz/gruen3fehler3.html')
+
+def gruen4(request):
+    return render(request, 'quiz/gruen4.html')
+
+def gruen4fehler1(request):
+    return render(request, 'quiz/gruen4fehler1.html')
+
+def gruen4fehler2(request):
+    return render(request, 'quiz/gruen4fehler2.html')
+
+def gruen4fehler3(request):
+    return render(request, 'quiz/gruen4fehler3.html')
+
+def rot(request):
+    return render(request, 'quiz/rot.html')
+
+def rotfehler1(request):
+    return render(request, 'quiz/rotfehler1.html')
+
+def rotfehler2(request):
+    return render(request, 'quiz/rotfehler2.html')
+
+def rotfehler3(request):
+    return render(request, 'quiz/rotfehler3.html')
+
+def rot2(request):
+    return render(request, 'quiz/rot2.html')
+
+def rot2fehler1(request):
+    return render(request, 'quiz/rot2fehler1.html')
+
+def rot2fehler2(request):
+    return render(request, 'quiz/rot2fehler2.html')
+
+def rot2fehler3(request):
+    return render(request, 'quiz/rot2fehler3.html')
+
+def rot3(request):
+    return render(request, 'quiz/rot3.html')
+
+def rot3fehler1(request):
+    return render(request, 'quiz/rot3fehler1.html')
+
+def rot3fehler2(request):
+    return render(request, 'quiz/rot3fehler2.html')
+
+def rot3fehler3(request):
+    return render(request, 'quiz/rot3fehler3.html')
+
+def rot4(request):
+    return render(request, 'quiz/rot4.html')
+
+def rot4fehler1(request):
+    return render(request, 'quiz/rot4fehler1.html')
+
+def rot4fehler2(request):
+    return render(request, 'quiz/rot4fehler2.html')
+
+def rot4fehler3(request):
+    return render(request, 'quiz/rot4fehler3.html')
+
+def rotglueck(request):
+    return render(request, 'quiz/rotglueck.html')
