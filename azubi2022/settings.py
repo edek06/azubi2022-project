77c7d4af5d3745788843978e2510ac0113fd6eac
89wozniak@gmail.com <89wozniak@gmail.com>
2022-08-26 10:37:47 +0200

    nach dem collectstatic

--- azubi2022/settings.py
+++ azubi2022/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'azubi2022/static/') # hier soll in dem Projekt in dem Ordner nach Datei gesucht
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
