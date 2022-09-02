e9ac61d04b99f67c067f47fd1ab7cf6123e42c20
89wozniak@gmail.com <89wozniak@gmail.com>
2022-09-02 14:17:11 +0200

    Antworten auf 100 Zecihen erweitern

--- quiz/models.py
+++ quiz/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Quiz(models.Model):
     kategorie = models.CharField(max_length=50)
     frage = models.CharField(max_length=200)
-    antwort1 = models.CharField(max_length=50)
-    antwort2 = models.CharField(max_length=50)
-    antwort3 = models.CharField(max_length=50)
-    antwort4 = models.CharField(max_length=50)
-    richtig = models.CharField(max_length=50)
+    antwort1 = models.CharField(max_length=100)
+    antwort2 = models.CharField(max_length=100)
+    antwort3 = models.CharField(max_length=100)
+    antwort4 = models.CharField(max_length=100)
+    richtig = models.CharField(max_length=100)
