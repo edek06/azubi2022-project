3fc03105a0a370a37f5059eac0300cc4db8bc259
89wozniak@gmail.com <89wozniak@gmail.com>
2022-09-02 15:25:24 +0200

    black Kategorie ist auch fertig

--- quiz/views.py
+++ quiz/views.py
@@ -114,6 +114,74 @@ def summary2(request, liste, antwort):
 
     return render(request, 'quiz/summary2.html', {'antworten':antworten, 'summary':summary})
 
+def black(request):
+    pos = Quiz.objects.get(pk=9)
+    return render(request, 'quiz/black.html', {'pos':pos})
+
+def black2(request, antwort):
+    antworten = []
+    antworten.append(antwort)
+    pos = Quiz.objects.get(pk=10)
+    return render(request, 'quiz/black2.html', {'pos':pos, 'antworten':antworten})
+
+def black3(request, liste, antwort):
+    antworten = []
+    temp_liste = len(liste)
+    antworten.append(liste[2:temp_liste - 2])
+    antworten.append(antwort)
+    pos = Quiz.objects.get(pk=11)
+    return render(request, 'quiz/black3.html', {'pos':pos, 'antworten':antworten})
+
+def black4(request, liste, antwort):
+    antworten = []
+    antwort1, antwort2 = liste.split(',')
+    antworten.append(antwort1[2:len(antwort1)-1])
+    antworten.append(antwort2[2:len(antwort2)-2])
+    antworten.append(antwort)
+    pos = Quiz.objects.get(pk=12)
+    return render(request, 'quiz/black4.html', {'pos':pos, 'antworten':antworten})
+
+def black5(request, liste, antwort):
+    antworten = []
+    antwort1, antwort2, antwort3 = liste.split(',')
+    antworten.append(antwort1[2:len(antwort1)-1])
+    antworten.append(antwort2[2:len(antwort2)-1])
+    antworten.append(antwort3[2:len(antwort3)-2])
+    antworten.append(antwort)
+    pos = Quiz.objects.get(pk=13)
+    return render(request, 'quiz/black5.html', {'pos':pos, 'antworten':antworten})
+
+def summary3(request, liste, antwort):
+    antworten = []
+    antworten_richtig = []
+    summary = {}
+
+    antwort1, antwort2, antwort3, antwort4 = liste.split(',')
+    antworten.append(antwort1[2:len(antwort1)-1])
+    antworten.append(antwort2[2:len(antwort2)-1])
+    antworten.append(antwort3[2:len(antwort3)-1])
+    antworten.append(antwort4[2:len(antwort4)-2])
+    antworten.append(antwort)
+
+    pos1 = Quiz.objects.get(pk=9)
+    antworten_richtig.append(pos1.richtig)
+    pos2 = Quiz.objects.get(pk=10)
+    antworten_richtig.append(pos2.richtig)
+    pos3 = Quiz.objects.get(pk=11)
+    antworten_richtig.append(pos3.richtig)
+    pos4 = Quiz.objects.get(pk=12)
+    antworten_richtig.append(pos4.richtig)
+    pos5 = Quiz.objects.get(pk=13)
+    antworten_richtig.append(pos5.richtig)
+
+    for i in range(5):
+        if antworten[i] == antworten_richtig[i]:
+            summary[antworten[i]] = "Richtig"
+        else:
+            summary[antworten[i]] = "Falsch"
+
+    return render(request, 'quiz/summary3.html', {'antworten':antworten, 'summary':summary})
+
 def schwarzfehler1(request):
     return render(request, 'quiz/schwarzfehler1.html')
 
